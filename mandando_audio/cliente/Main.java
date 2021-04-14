d5e3d4749ef9895c39a2a07efc5d4d90dfe511e7
Arthur Nakao <arthur.nakao@gmail.com>
2021-04-13 23:17:15 -0300

    Apagando pastas desnecessarias

--- mandando_audio/cliente/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-        GreetClient client = new GreetClient();
-        client.startConnection("127.0.0.1", 6666);
-        // String response = client.sendMessage("hello server");
-        client.sendMessage("hello server");
-        System.out.println("Mensagem enviada");
-    }
-}
